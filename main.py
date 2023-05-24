0e3163f84b04ccb1543e8f6556af7d2e145c9f92
joao victor gomes <joao-iccz@hotmail.com>
2023-05-24 09:21:13 -0300

    Conversão de arquivo, erro em converter

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, request, flash, redirect
-import json
 import os
+from flask import Flask, render_template, request, flash, redirect, send_file
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
+from PyPDF2 import PdfWriter
+from docx2pdf import convert
+import pyexcel as pe
+import subprocess
+import json
 
+
+UPLOAD_FOLDER = 'arquivos'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 app = Flask(__name__)
-app.config['SECRET_KEY']= "joao123"
+app.config['SECRET_KEY'] = "joao123"
+app.config['UPLOAD_FOLDER'] = 'arquivos'
+
 @app.route("/")
 def home():
     return render_template("html/login.html")
@@ -15,16 +27,164 @@ def login():
     senha = request.form.get('senha')
     with open('usuarios.json') as usuarios:
         lista = json.load(usuarios)
-        cont = 0
         for c in lista:
-            cont+=1
-            if usuario == c['nome'] and senha == c['senha']:
+            if usuario == c['nome'] and check_password_hash(c['senha'], senha):
                 return render_template("html/acesso.html", nomeUsuario=c['nomeCompleto'])
-            if cont >= len(lista):
-                flash('Acesso Negado')
-                return redirect("/")
+        flash('Acesso Negado')
+        return redirect("/")
+
+@app.route("/acesso")
+def acesso():
+    return render_template("html/acesso.html")
 
 
+@app.route("/arquivo", methods=['GET', 'POST'])
+def arquivo():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('Nenhum arquivo selecionado.')
+            return redirect("/arquivo")
 
-if __name__ in '__main__':
-    app.run(debug=True)
+        arquivo = request.files['file']
+
+        if arquivo.filename == '':
+            flash('Nenhum arquivo selecionado.')
+            return redirect("/arquivo")
+
+        filename = secure_filename(arquivo.filename)
+        arquivo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        pdf_filename = convert_to_pdf(filename)
+
+        if pdf_filename:
+            # Retorna o nome do arquivo PDF para a mesma página
+            return render_template("html/arquivo.html", pdf_filename=pdf_filename)
+        else:
+            flash('Erro ao converter arquivo para PDF.')
+            return redirect("/arquivo")
+
+    return render_template("html/arquivo.html")
+
+def converter_pdf():
+    if 'file' not in request.files:
+        flash('Nenhum arquivo selecionado.')
+        return redirect("/arquivo")
+
+    arquivo = request.files['file']
+
+    if arquivo.filename == '':
+        flash('Nenhum arquivo selecionado.')
+        return redirect("/arquivo")
+
+    filename = secure_filename(arquivo.filename)
+    arquivo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    extensao = os.path.splitext(filename)[1].lower()
+    pdf_filename = filename.replace(extensao, '.pdf')
+    pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], pdf_filename)
+
+    if extensao == '.txt':
+        txt_to_pdf(os.path.join(app.config['UPLOAD_FOLDER'], filename), pdf_path)
+    elif extensao == '.docx':
+        docx_to_pdf(os.path.join(app.config['UPLOAD_FOLDER'], filename), pdf_path)
+    elif extensao == '.xlsx':
+        xlsx_to_pdf(os.path.join(app.config['UPLOAD_FOLDER'], filename), pdf_path)
+
+    if os.path.isfile(pdf_path):
+        # Retorna o arquivo PDF como um download
+        return send_file(pdf_path, as_attachment=True)
+    else:
+        flash('Erro ao converter arquivo para PDF.')
+        return redirect("/arquivo")
+
+
+@app.route("/download_pdf", methods=['POST'])
+def download_pdf():
+    pdf_filename = request.form.get('pdf_filename')
+    pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], pdf_filename)
+
+    if os.path.isfile(pdf_path):
+        return send_file(pdf_path, as_attachment=True)
+    else:
+        flash('Erro ao fazer o download do arquivo PDF.')
+        return redirect("/arquivo")
+
+
+@app.route("/audio")
+def audio():
+    return render_template("html/audio.html")
+
+@app.route("/imagem")
+def imagem():
+    return render_template("html/imagem.html")
+
+@app.route("/cadastrar", methods=['GET', 'POST'])
+def cadastrar():
+    if request.method == 'POST':
+        usuario = request.form.get('nome')
+        senha = request.form.get('senha')
+        nome_completo = request.form.get('nome_completo')
+
+        # Codifica a senha
+        senha_codificada = generate_password_hash(senha)
+
+        # Lê o arquivo JSON existente
+        with open('usuarios.json') as usuarios_file:
+            usuarios = json.load(usuarios_file)
+
+        # Adiciona o novo usuário
+        novo_usuario = {
+            'nome': usuario,
+            'senha': senha_codificada,
+            'nomeCompleto': nome_completo
+        }
+        usuarios.append(novo_usuario)
+
+        # Salva os usuários no arquivo JSON
+        with open('usuarios.json', 'w') as usuarios_file:
+            json.dump(usuarios, usuarios_file, indent=4)
+
+        flash('Cadastro realizado com sucesso!')
+        return redirect("/")
+    else:
+        return render_template("html/cadastro.html")
+
+def convert_to_pdf(filename):
+    extensao = os.path.splitext(filename)[1].lower()
+    pdf_filename = filename.replace(extensao, '.pdf')
+    pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], pdf_filename)
+
+    if extensao == '.txt':
+        txt_to_pdf(os.path.join(app.config['UPLOAD_FOLDER'], filename), pdf_path)
+    elif extensao == '.docx':
+        docx_to_pdf(os.path.join(app.config['UPLOAD_FOLDER'], filename), pdf_path)
+    elif extensao == '.xlsx':
+        xlsx_to_pdf(os.path.join(app.config['UPLOAD_FOLDER'], filename), pdf_path)
+
+    if os.path.isfile(pdf_path):
+        return pdf_filename
+    else:
+        return None
+
+def txt_to_pdf(txt_path, pdf_path):
+    with open(txt_path, 'r') as txt_file, open(pdf_path, 'wb') as pdf_file:
+        pdf_writer = PdfWriter()
+        pdf_writer.add_page()
+        pdf_writer.add_font('Arial', '', 'static/fonts/arial.ttf')
+        pdf_writer.set_font('Arial', '', 12)
+        for line in txt_file:
+            pdf_writer.write_line(line.strip())
+        pdf_writer.save(pdf_file)
+
+def docx_to_pdf(docx_path, pdf_path):
+    command = ['unoconv', '-f', 'pdf', '-o', pdf_path, docx_path]
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Erro ao converter arquivo DOCX para PDF: {e}")
+
+def xlsx_to_pdf(xlsx_path, pdf_path):
+    sheet = pe.get_sheet(file_name=xlsx_path)
+    sheet.save_as(pdf_path)
+
+if __name__ == '__main__':
+    app.run()
