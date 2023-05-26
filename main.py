0d7fcaf199d5f8e99d49f89a2c1e2772fb233dae
joao victor gomes <joao-iccz@hotmail.com>
2023-05-26 11:45:13 -0300

    ajustes

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ from flask import Flask, render_template, request, flash, redirect, send_file
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 from PyPDF2 import PdfWriter
-from docx2pdf import convert
+from docx import Document
 import pyexcel as pe
-import subprocess
 import json
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from docx import Document
+import subprocess
+from flask import send_file
+
 
 
 UPLOAD_FOLDER = 'arquivos'
@@ -17,9 +22,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "joao123"
 app.config['UPLOAD_FOLDER'] = 'arquivos'
 
+
 @app.route("/")
 def home():
     return render_template("html/login.html")
+
 
 @app.route("/login", methods=['POST'])
 def login():
@@ -32,6 +39,7 @@ def login():
                 return render_template("html/acesso.html", nomeUsuario=c['nomeCompleto'])
         flash('Acesso Negado')
         return redirect("/")
+
 
 @app.route("/acesso")
 def acesso():
@@ -64,38 +72,6 @@ def arquivo():
 
     return render_template("html/arquivo.html")
 
-def converter_pdf():
-    if 'file' not in request.files:
-        flash('Nenhum arquivo selecionado.')
-        return redirect("/arquivo")
-
-    arquivo = request.files['file']
-
-    if arquivo.filename == '':
-        flash('Nenhum arquivo selecionado.')
-        return redirect("/arquivo")
-
-    filename = secure_filename(arquivo.filename)
-    arquivo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-    extensao = os.path.splitext(filename)[1].lower()
-    pdf_filename = filename.replace(extensao, '.pdf')
-    pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], pdf_filename)
-
-    if extensao == '.txt':
-        txt_to_pdf(os.path.join(app.config['UPLOAD_FOLDER'], filename), pdf_path)
-    elif extensao == '.docx':
-        docx_to_pdf(os.path.join(app.config['UPLOAD_FOLDER'], filename), pdf_path)
-    elif extensao == '.xlsx':
-        xlsx_to_pdf(os.path.join(app.config['UPLOAD_FOLDER'], filename), pdf_path)
-
-    if os.path.isfile(pdf_path):
-        # Retorna o arquivo PDF como um download
-        return send_file(pdf_path, as_attachment=True)
-    else:
-        flash('Erro ao converter arquivo para PDF.')
-        return redirect("/arquivo")
-
 
 @app.route("/download_pdf", methods=['POST'])
 def download_pdf():
@@ -108,6 +84,33 @@ def download_pdf():
         flash('Erro ao fazer o download do arquivo PDF.')
         return redirect("/arquivo")
 
+def convert_to_pdf(filename):
+    extensao = os.path.splitext(filename)[1].lower()
+    pdf_filename = filename.replace(extensao, '.pdf')
+    pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], pdf_filename)
+
+    if extensao == '.txt':
+        txt_to_pdf(os.path.join(app.config['UPLOAD_FOLDER'], filename), pdf_path)
+    elif extensao == '.docx':
+        if convert_docx_to_pdf(os.path.join(app.config['UPLOAD_FOLDER'], filename), pdf_path):
+            return pdf_filename
+        else:
+            return None
+    elif extensao == '.xlsx':
+        xlsx_to_pdf(os.path.join(app.config['UPLOAD_FOLDER'], filename), pdf_path)
+
+    if os.path.isfile(pdf_path):
+        return pdf_filename
+    else:
+        return None
+
+def convert_docx_to_pdf(docx_path, pdf_path):
+    try:
+        subprocess.run(['/usr/bin/libreoffice', '--headless', '--convert-to', 'pdf', '--outdir', pdf_path, docx_path],
+         check=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
 
 @app.route("/audio")
 def audio():
@@ -117,53 +120,6 @@ def audio():
 def imagem():
     return render_template("html/imagem.html")
 
-@app.route("/cadastrar", methods=['GET', 'POST'])
-def cadastrar():
-    if request.method == 'POST':
-        usuario = request.form.get('nome')
-        senha = request.form.get('senha')
-        nome_completo = request.form.get('nome_completo')
-
-        # Codifica a senha
-        senha_codificada = generate_password_hash(senha)
-
-        # Lê o arquivo JSON existente
-        with open('usuarios.json') as usuarios_file:
-            usuarios = json.load(usuarios_file)
-
-        # Adiciona o novo usuário
-        novo_usuario = {
-            'nome': usuario,
-            'senha': senha_codificada,
-            'nomeCompleto': nome_completo
-        }
-        usuarios.append(novo_usuario)
-
-        # Salva os usuários no arquivo JSON
-        with open('usuarios.json', 'w') as usuarios_file:
-            json.dump(usuarios, usuarios_file, indent=4)
-
-        flash('Cadastro realizado com sucesso!')
-        return redirect("/")
-    else:
-        return render_template("html/cadastro.html")
-
-def convert_to_pdf(filename):
-    extensao = os.path.splitext(filename)[1].lower()
-    pdf_filename = filename.replace(extensao, '.pdf')
-    pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], pdf_filename)
-
-    if extensao == '.txt':
-        txt_to_pdf(os.path.join(app.config['UPLOAD_FOLDER'], filename), pdf_path)
-    elif extensao == '.docx':
-        docx_to_pdf(os.path.join(app.config['UPLOAD_FOLDER'], filename), pdf_path)
-    elif extensao == '.xlsx':
-        xlsx_to_pdf(os.path.join(app.config['UPLOAD_FOLDER'], filename), pdf_path)
-
-    if os.path.isfile(pdf_path):
-        return pdf_filename
-    else:
-        return None
 
 def txt_to_pdf(txt_path, pdf_path):
     with open(txt_path, 'r') as txt_file, open(pdf_path, 'wb') as pdf_file:
@@ -175,16 +131,32 @@ def txt_to_pdf(txt_path, pdf_path):
             pdf_writer.write_line(line.strip())
         pdf_writer.save(pdf_file)
 
-def docx_to_pdf(docx_path, pdf_path):
-    command = ['unoconv', '-f', 'pdf', '-o', pdf_path, docx_path]
+def convert_docx_to_pdf(docx_path, pdf_path):
     try:
-        subprocess.run(command, check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Erro ao converter arquivo DOCX para PDF: {e}")
+        subprocess.run(['/usr/bin/libreoffice', '--headless', '--convert-to', 'pdf', '--outdir', pdf_path, docx_path],
+         check=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+def docx_to_pdf(docx_path, pdf_path):
+    doc = Document(docx_path)
+    paragraphs = [p.text for p in doc.paragraphs]
+    content = "\n".join(paragraphs)
+
+    c = canvas.Canvas(pdf_path)
+    c.setFont('Arial', 12)
+    text = c.beginText(50, 750)
+    text.setFont('Arial', 12)
+    text.textLines(content)
+    c.drawText(text)
+    c.save()
+
 
 def xlsx_to_pdf(xlsx_path, pdf_path):
     sheet = pe.get_sheet(file_name=xlsx_path)
     sheet.save_as(pdf_path)
+
 
 if __name__ == '__main__':
     app.run()
